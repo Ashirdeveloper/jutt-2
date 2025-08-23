512a87bf7a73b320a02dacc5d8949a10337d7753
Ashirdeveloper <162281442+Ashirdeveloper@users.noreply.github.com>
2025-08-23 04:13:29 -0700

    Create create-table.sql

---
+++ create-table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Students (
+    student_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    gender VARCHAR(10),
+    grade VARCHAR(5),
+    email VARCHAR(100),
+    phone VARCHAR(15),
+    address VARCHAR(150)
+);
